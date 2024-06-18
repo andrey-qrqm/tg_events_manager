648d5e497580b41f588133b62b426a36213e9bf9
Andrey QRQM <andrey.madyarov@gmail.com>
2024-06-18 17:41:18 +0300

    Optimization idea commit

--- tele_main.py
+++ tele_main.py
@@ -12,19 +12,19 @@ bot = telebot.TeleBot(token)
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
     message.text = message.text.lower()
-    """
+
     if message.text.startswith("event"): 
         try:
             days = int(message.text[6:])
             EventsReply = reply_generator(days)
-        except TypeError:
+        except ValueError:
             EventsReply = "This command is not correct, type help to see correct commands"
         if EventsReply != "":
             bot.send_message(message.from_user.id, EventsReply)
         else:
             EventsReply = "Currently there are no events at this date"
             bot.send_message(message.from_user.id, EventsReply)
-       """
+        """
     if message.text == "event":
         EventsReply = reply_generator(1)
         if EventsReply != "":
@@ -47,14 +47,14 @@ def get_text_messages(message):
             EventsReply = "Currently there are no events at this date"
             bot.send_message(message.from_user.id, EventsReply)
     else:
-        bot.send_message(message.from_user.id, """
+        bot.send_message(message.from_user.id, 
         This command does not exist.
         Available commands:
         event - Events for today
         event 3 - Events for 3 days
         event 7 - Events for a week
-        """)
-
+        )
+"""
 
 if __name__ == '__main__':
     bot.polling(none_stop=True, interval=5)
